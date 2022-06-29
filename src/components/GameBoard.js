f7ab6b61c9aaaf4a72e1b90a892a47914cc68a93
Mahnoor <mahnoorali57se@gmail.com>
2022-06-30 01:28:36 +0500

    fixed bugs

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,141 +1,108 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ".//GameBoard.css";
 
 function GameBoard() {
-  const [symbol, setSymbol] = useState("X");
-  const [board, setBoard] = useState(Array(9).fill(""));
-  const [result, setResult] = useState(".");
+    const [symbol, setSymbol] = useState("X");
+    const [board, setBoard] = useState(Array(9).fill(""));
+    const [result, setResult] = useState(".");
 
-  function checkWinner() {
-    //Check vertical win
-    for (let i = 0; i < 3; i++) {
-      //   console.log("1:  ");
-      //   console.log(
-      //     board[0] +
-      //       ", " +
-      //       board[1] +
-      //       ", " +
-      //       board[2] +
-      //       ", " +
-      //       board[3] +
-      //       ", " +
-      //       board[4] +
-      //       " ," +
-      //       board[5] +
-      //       ", " +
-      //       board[6] +
-      //       ", " +
-      //       board[7] +
-      //       ", " +
-      //       board[8]
-      //   );
-      if (((board[i] === board[i + 3]) === board[i + 6]) === "X") {
-        setResult("X");
-        // console.log("1");
-      } else if (((board[i] === board[i + 3]) === board[i + 6]) === "O") {
-        // console.log("1.1");
-        setResult("O");
-      }
+    function checkWinner() {
+        // if(!result==="") //if there is a winner, no need to check
+        
+        //Check vertical win
+        for (let i = 0; i < 3; i++) {
+            // console.log("1:  ");
+         
+            if ((board[i] === board[i + 3]) && (board[i + 3] === board[i + 6])) {
+                setResult(board[i]);
+                console.log("vertical");
+            }
+        }
+
+        //Check diagnol win
+        if ((board[0] === board[4]) && (board[4] === board[8])) {
+            setResult(board[0]);
+            console.log("diagonal");
+        } 
+        else if((board[2] === board[4]) && (board[4] === board[6]))
+        {
+            setResult(board[2]);
+            console.log("diagonal");
+        }
+
+        //Check horizontal win
+        for (let i = 0; i < 7; i += 3) {
+            // console.log("2:  ");
+            //
+            if ((board[i] === board[i + 1]) && (board[i + 1] === board[i + 2])) {
+                setResult(board[i]);
+                console.log("horizontal");
+            }
+        }
+        console.log("Inside checkWinner " + result);
+
+        if (!board.includes("") && result === "") {
+            setResult("Its Draw!");
+        }
     }
 
-    //Check diagnol win
-    if (
-      ((board[0] === board[4]) === board[8]) === "X" ||
-      ((board[2] === board[4]) === board[6]) === "X"
-    ) {
-      setResult("X");
-      //   console.log("2");
-    } else if (
-      ((board[0] === board[4]) === board[8]) === "O" ||
-      ((board[2] === board[4]) === board[6]) === "O"
-    ) {
-      setResult("O");
-      //   console.log("2.1");
+    function handleClick(boardCell) {
+        if((board[boardCell]==="")&&(result==="")) //if the cell is empty and there is no winner
+        {
+        let newBoard = [...board];
+        setSymbol(symbol === "X" ? "O" : "X");
+        newBoard[boardCell] = symbol;
+        setBoard(newBoard);
+        console.log(board);
+        if(result==="")
+        {
+        checkWinner();
+        }
+        }
     }
 
-    //Check horizontal win
-    for (let i = 0; i < 7; i += 3) {
-      //   console.log("2:  ");
-      //   console.log(
-      //     board[0] +
-      //       ", " +
-      //       board[1] +
-      //       ", " +
-      //       board[2] +
-      //       ", " +
-      //       board[3] +
-      //       ", " +
-      //       board[4] +
-      //       " ," +
-      //       board[5] +
-      //       ", " +
-      //       board[6] +
-      //       ", " +
-      //       board[7] +
-      //       ", " +
-      //       board[8]
-      //   );
+    useEffect( () => {
+        checkWinner();
+    }, [board]);
 
-      if (((board[i] === board[i + 1]) === board[i + 2]) === "X") {
-        setResult("X");
-        // console.log("3");
-      } else if (((board[i] === board[i + 1]) === board[i + 2]) === "O") {
-        setResult("O");
-        // console.log("3.1");
-      }
-    }
-    // console.log("Inside checkWinner " + result);
+    const Cell = (props) => {
+        const val = props.num;
+        return <td onClick={() => handleClick(val)}>{board[props.num]}</td>;
+    };
 
-    if (!board.includes("") && result === "") {
-      setResult("Its Draw!");
-    }
-  }
+    const Winner = () => {
+        return <h4>Game Winner: {result} </h4>;
+    };
 
-  function handleClick(boardCell) {
-    let newBoard = [...board];
-    setSymbol(symbol === "X" ? "O" : "X");
-    newBoard[boardCell] = symbol;
-    console.log(newBoard);
-    setBoard(newBoard);
-    checkWinner();
-  }
-
-  const Cell = (props) => {
-    const val = props.num;
-    return <td onClick={() => handleClick(val)}>{board[props.num]}</td>;
-  };
-
-  const Winner = () => {
-    return <h4>Game Winner: {result} </h4>;
-  };
-
-  return (
-    <>
-      <h3>Player {symbol} turn </h3>
-      <div className="container">
-        <table>
-          <tbody>
-            <tr>
-              <Cell num="0" />
-              <Cell num="1" />
-              <Cell num="2" />
-            </tr>
-            <tr>
-              <Cell num="3" />
-              <Cell num="4" />
-              <Cell num="5" />
-            </tr>
-            <tr>
-              <Cell num="6" />
-              <Cell num="7" />
-              <Cell num="8" />
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <Winner />
-    </>
-  );
+    return (
+        <>
+            <h3>Player {symbol} turn </h3>
+            <div className="container">
+                <table>
+                    <tbody>
+                        <tr>
+                            <Cell num="0" />
+                            <Cell num="1" />
+                            <Cell num="2" />
+                        </tr>
+                        <tr>
+                            <Cell num="3" />
+                            <Cell num="4" />
+                            <Cell num="5" />
+                        </tr>
+                        <tr>
+                            <Cell num="6" />
+                            <Cell num="7" />
+                            <Cell num="8" />
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div id="result">
+            <Winner />
+            </div>
+        </>
+    );
 }
 
 export default GameBoard;
